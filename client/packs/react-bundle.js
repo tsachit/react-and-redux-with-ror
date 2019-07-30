bb05cd74cd6954f64222861ca09caa35ed202e42
Sachit Karki <sachitjungkarki@gmail.com>
2019-07-30 09:46:42 +0545

    Initial setup with react, redux and ror complete

--- client/packs/react-bundle.js
+++ client/packs/react-bundle.js
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import App from '../bundles/HelloWorld/App';
+import App from '../App';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
